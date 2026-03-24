61584b37428d2c8da06ba0634c89b329d733830e
MANOHAR SORAPALLI <alwayzmanu@gmail.com>
2026-03-24 14:28:50 +0530

    Backend applied to Auth

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,26 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api", // 🔥 CHANGE THIS
+  baseURL: "http://localhost:5000/api",
   timeout: 10000,
 });
 
-// OPTIONAL (for auth later)
-api.interceptors.request.use((config) => {
-  // you can attach token later if needed
-  return config;
-});
+//  ATTACH TOKEN TO EVERY REQUEST
+api.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
